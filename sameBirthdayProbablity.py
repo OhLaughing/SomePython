5958c0262369d439782ea4bdc60dc1aacf15d2e9
OhLaughing <bigwanggang@163.com>
2020-04-22 09:25:11 +0800

    Create sameBirthdayProbablity.py

---
+++ sameBirthdayProbablity.py
@@ -0,0 +1,27 @@
+import math
+from matplotlib import pyplot as plt
+
+# 阶乘
+def func(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return (n * func(n - 1))
+
+
+# 排列的可能性
+def permutation(a, b):
+    return func(a) / func(a - b)
+
+
+num = []
+probablity = []
+for i in range(100):
+    num.append(i)
+    a = 1 - permutation(365, i) / math.pow(365, i)
+    probablity.append(a)
+
+# probablity = [1- (permutation(365, i) / math.pow(365, i)) for i in num]
+
+plt.plot(num, probablity, '-')
+plt.show()
