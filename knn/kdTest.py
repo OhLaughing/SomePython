fa63adc277ef7c54ffc0109a79b88e6f10186555
bigwanggang <bigwanggang@163.com>
2020-08-24 22:09:27 +0800

    update knn

--- knn/kdTest.py
+++ knn/kdTest.py
@@ -5,6 +5,7 @@ from knn.kd_node import Node
 if __name__ == '__main__':
     T = np.array([[2, 3], [5, 4], [9, 6], [4, 7], [8, 1], [7, 2]])
     sort = T.argsort(axis=0)
+    head = Node(None,0,None)
+    node = Node(T,1,head)
+    node.init()
 
-    node = Node(T,1)
-    node.constructTree()
--- knn/kd_node.py
+++ knn/kd_node.py
@@ -1,19 +1,26 @@
 import numpy as np
+
+
 class Node:
-    def __init__(self, allPoints, level):
+    def __init__(self, allPoints, level, parent):
         self.allPoints = allPoints
         self.level = level
+        self.parent = parent
         self.point = 0
         self.left = 0
         self.right = 0
-        self.parent
-        self.pointIndex = []
 
-    def constructTree(self):
+    def init(self):
         sort = np.argsort(self.allPoints)
         var = np.var(self.allPoints, axis=0)
-        dividDim = np.argsort(var)[var.shape[0]-1]
-        sort = np.argsort(self.allPoints, axis=0)[:,dividDim]
-        middleP = self.allPoints
+        dividDim = np.argsort(var)[var.shape[0] - 1]
+        sort = np.argsort(self.allPoints, axis=0)[:, dividDim]
+        l = int(len(sort) / 2)
+        middleP = self.allPoints[sort[l]]
+        self.point = middleP
+        self.left = Node(self.allPoints, self.level + 1, self)
+        self.right = Node(self.allPoints, self.level + 1, self)
+        self.left.init()
+        self.right.init()
         # 在一个维度上分割数据之后，下一次分割的维度怎么算（要不要还算父分割的维度）
         print(dividDim)
