bdfddaedcda2c48ff23eeb05ec0e9ed230ad3058
OhLaughing <bigwanggang@163.com>
2020-08-28 08:18:35 +0800

    Update dating_kdTree.py

--- knn/dating_kdTree.py
+++ knn/dating_kdTree.py
@@ -7,19 +7,19 @@ if __name__ == '__main__':
     filename = "datingTestSet.txt"
     # 打开并处理数据
     datingDataMat, datingLabels = knn.file2matrix(filename)
-    trainData = datingDataMat[100:,:]
     trainlabels = datingLabels[100:]
-    testData = datingDataMat[0:100,:]
     testlabels = datingLabels[0:100]
     # 训练集归一化
-    normDataset, ranges, minVals = knn.autoNorm(trainData)
-    T = np.c_[normDataset, np.arange(normDataset.shape[0])]
+    normDataset, ranges, minVals = knn.autoNorm(datingDataMat)
+    trainData = normDataset[100:, :]
+    testData = normDataset[0:100, :]
+    T = np.c_[trainData, np.arange(trainData.shape[0])]
     tree = kd.initKdTree(T, 1, None, T.shape[1])
     kd.printNode(tree)
 
     errorNum =0
     for i in range(len(testlabels)):
-        nearNodeList = kd.findkNearestNode(tree, testData[i, :], 3)
+        nearNodeList = kd.findkNearestNode(tree, testData[i, :], 5)
         nearestLabel = kd.getNearestLabel(nearNodeList, trainlabels)
         if(nearestLabel != testlabels[i]):
             errorNum+=1
