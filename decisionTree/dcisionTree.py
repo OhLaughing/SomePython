86542a5ac18e9517a1da7fb849e5aaac20e50b07
OhLaughing <bigwanggang@163.com>
2020-09-05 12:50:38 +0800

    Update dcisionTree.py

--- decisionTree/dcisionTree.py
+++ decisionTree/dcisionTree.py
@@ -1,6 +1,16 @@
 import math
 from decisionTree.watermelon import getData
 import numpy as np
+
+def getEnt(data):
+    ent = 0.0
+    totalnum =0
+    for key, num in data.items():
+        totalnum+=num
+    for key, num in data.items():
+        ent += num * math.log(num / totalnum, 2)
+    return -ent/totalnum, totalnum
+
 def entropy(labels, indexList):
     total = len(indexList)
     classification = {}
@@ -10,21 +20,43 @@ def entropy(labels, indexList):
         else:
             classification[labels[i]] = 1.0
 
-    ent = 0.0
-    for key, num in classification.items():
-        ent += num*math.log(2, float(num / total))
+    ent = getEnt(classification)
+    return ent
 
-    return -ent/total
+def put(total, data, label):
+    d = total[data]
+    if label in d:
+        d[label]+=1
+    else:
+        d[label]=1
 
 # 计算各个属性的信息熵
 def information_entropy(properties, labels, indexList):
     total ={}
     for i in indexList:
-        if properties[i] in total:
+        if properties[i] not in total:
+            total[properties[i]] = {}
+        put(total, properties[i], labels[i])
 
-        else:
+    print(total)
 
-    # pass
+    each_ent = []
+    each_num =[]
+    for key, value in total.items():
+        ent, num = getEnt(value)
+        each_ent.append(ent)
+        each_num.append(num)
+
+    return each_ent, each_num
+
+
+def information_gain(ent, each_ent, each_num):
+    sum1 = sum(each_num)
+    s = 0.0
+    for i in range(len(each_num)):
+        s+= (each_num[i]/sum1)*each_ent[i]
+    return ent - s
+
 
 
 if __name__ == '__main__':
@@ -32,5 +64,15 @@ if __name__ == '__main__':
     print(data)
     print(labels)
     indexList = np.arange(len(labels))
-    ent = entropy(labels,indexList)
+    ent, totalNum = entropy(labels,indexList)
     print(ent)
+    info_gains=[]
+    for i in range(data.shape[1]):
+        each_ent, each_num = information_entropy(data[:,i], labels, indexList)
+        print(each_ent)
+        print(each_num)
+        print(sum(each_num))
+        info_gain = information_gain(ent, each_ent, each_num)
+        info_gains.append(info_gain)
+        print(info_gain)
+    print(info_gains)
