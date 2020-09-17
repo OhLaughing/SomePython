63735eec18c57fbdebf5b8ca87d02236e4f4b397
OhLaughing <bigwanggang@163.com>
2020-09-17 20:23:01 +0800

    Update dcisionTree.py

--- decisionTree/dcisionTree.py
+++ decisionTree/dcisionTree.py
@@ -1,15 +1,16 @@
 import math
-from decisionTree.watermelon import getData
+
 import numpy as np
 
-def getEnt(data):
+from decisionTree.watermelon import getData
+
+
+def getEnt(data, totalnum):
     ent = 0.0
-    totalnum =0
-    for key, num in data.items():
-        totalnum+=num
     for key, num in data.items():
         ent += num * math.log(num / totalnum, 2)
-    return -ent/totalnum, totalnum
+    return -ent / totalnum
+
 
 def entropy(labels, indexList):
     total = len(indexList)
@@ -20,19 +21,28 @@ def entropy(labels, indexList):
         else:
             classification[labels[i]] = 1.0
 
-    ent = getEnt(classification)
+    ent = getEnt(classification, len(indexList))
     return ent
+
 
 def put(total, data, label):
     d = total[data]
     if label in d:
-        d[label]+=1
+        d[label] += 1
     else:
-        d[label]=1
+        d[label] = 1
+
+
+def getTotal(dict1):
+    total = 0
+    for _, num in dict1.items():
+        total += num
+    return total
+
 
 # 计算各个属性的信息熵
 def information_entropy(properties, labels, indexList):
-    total ={}
+    total = {}
     for i in indexList:
         if properties[i] not in total:
             total[properties[i]] = {}
@@ -41,11 +51,12 @@ def information_entropy(properties, labels, indexList):
     print(total)
 
     each_ent = []
-    each_num =[]
+    each_num = []
     for key, value in total.items():
-        ent, num = getEnt(value)
+        total = getTotal(value)
+        ent = getEnt(value, total)
         each_ent.append(ent)
-        each_num.append(num)
+        each_num.append(total)
 
     return each_ent, each_num
 
@@ -54,9 +65,78 @@ def information_gain(ent, each_ent, each_num):
     sum1 = sum(each_num)
     s = 0.0
     for i in range(len(each_num)):
-        s+= (each_num[i]/sum1)*each_ent[i]
+        s += (each_num[i] / sum1) * each_ent[i]
     return ent - s
 
+
+def getBestAttr(info_gains):
+    bigest = -1.0
+    index = -1
+    for k, v in info_gains:
+        if v > bigest:
+            bigest = v
+            index = k
+    return index
+
+
+def getSubIndex(theAttrList, dataIndexList):
+    t = {}
+    for i in dataIndexList:
+        if theAttrList[i] in t:
+            t[theAttrList[i]].append(i)
+        else:
+            t[theAttrList[i]] = []
+    return t
+
+
+def otherAttrList(attrIndexList, theIndex):
+    newIndex = []
+    for i in attrIndexList:
+        if theIndex != i:
+            newIndex.append(i)
+    return newIndex
+
+
+def getValueNum(labels, indexList):
+    numDict = {}
+    for i in indexList:
+        if labels[i] in numDict:
+            numDict[labels[i]] += 1
+        else:
+            numDict[labels[i]] = 1
+    return numDict
+
+
+def getLargestNum(numDict):
+    index = 0
+    largest = 0
+    for k, num in numDict.items():
+        if num > largest:
+            largest = num
+            index = k
+    return index
+
+
+def initDecisionTree(data, labels, attrIndexList, dataIndexList):
+    numDict = getValueNum(labels, dataIndexList)
+    if len(numDict) == 1:
+        return labels[dataIndexList[0]]
+    if len(attrIndexList) == 1:
+        return getLargestNum(numDict)
+    ent = entropy(labels, dataIndexList)
+    info_gains = {}
+    for i in attrIndexList:
+        each_ent, each_num = information_entropy(data[:, i], labels, indexList)
+        info_gain = information_gain(ent, each_ent, each_num)
+        info_gains[i] = info_gain
+    print(info_gains)
+    bestAttr = getBestAttr(info_gains)
+    otherAttrs = otherAttrList(attrIndexList, bestAttr)
+    subIndex = getSubIndex(data[:, bestAttr], dataIndexList)
+    nextTree = {}
+    for value, index in subIndex.items():
+        nextTree[value] = initDecisionTree(data, labels, otherAttrs, index)
+    return dict(bestAttr=nextTree)
 
 
 if __name__ == '__main__':
@@ -64,15 +144,8 @@ if __name__ == '__main__':
     print(data)
     print(labels)
     indexList = np.arange(len(labels))
-    ent, totalNum = entropy(labels,indexList)
+    ent = entropy(labels, indexList)
     print(ent)
-    info_gains=[]
-    for i in range(data.shape[1]):
-        each_ent, each_num = information_entropy(data[:,i], labels, indexList)
-        print(each_ent)
-        print(each_num)
-        print(sum(each_num))
-        info_gain = information_gain(ent, each_ent, each_num)
-        info_gains.append(info_gain)
-        print(info_gain)
-    print(info_gains)
+    info_gains = []
+    tree = initDecisionTree(data, labels, range(data.shape[1]), range(data.shape[0]))
+    print(tree)
